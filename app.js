299d10fed183bba2bfbbf11fa0fa9a2f0e513913
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-04 10:15:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+function findIndex(obj, word){
+  for(i = 0 ; i < obj.length ; i++){
+    if(obj[i].name == word){
+      console.log(i)
+      break
+    }
+  }
+}
+const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+// expexted result: 1
